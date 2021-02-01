72bc21b76ebbc7d630bd6f502283302b94b6f284
ADx96 <76255763+ADx96@users.noreply.github.com>
2021-02-01 11:00:49 +0300

    Solved Task2

---
+++ Task2.js
@@ -0,0 +1,5 @@
+const name1 = "Abduljawad";
+let age = 25;
+let hobby = "BodyBuilding";
+console.log('Mr.' + name1 +  "in 33 years, you will be " + (age+33) + " years old. By then youll have decades of " + hobby + " Practice " );
+
